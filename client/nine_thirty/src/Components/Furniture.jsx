d79d7d719624c197f8edec710fadfda01a64303f
Maylis <maylis.ameto@gmail.com>
2023-06-28 15:25:45 +0200

    New component Furniture

---
+++ client/nine_thirty/src/Components/Furniture.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Furniture () {
+    return (
+        <div>
+            <img  />{/*photo*/}
+            <h1>{/*furniture name*/}</h1>
+            <p>{/*description*/}</p>
+            <p>{/*material*/}</p>
+            <p>{/*price*/}</p>
+            <ul>{/*dimensions/sizes*/}</ul>
+            <button>{/*add to cart*/}</button>
+        </div>
+    );
+};
+
+export default Furniture;
