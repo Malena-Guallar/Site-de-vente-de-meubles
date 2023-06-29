c674ca9c3d0700d6aa3eef540b8cf524f3952a3b
Maylis <maylis.ameto@gmail.com>
2023-06-29 14:13:58 +0200

    useless commit

--- client/nine_thirty/src/Components/Furniture.jsx
+++ client/nine_thirty/src/Components/Furniture.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Furniture ({ picture, type, description, price, material, size_height, size_width, size_deep }) {
+function Furniture ({ type, price }) {
+
+    getProducts();
 
     return (
         <div>
-            <p>ceci est un meuble</p>
-            <img src={picture} alt="products"/>
-            <h1>{type}</h1>
-            <p>{description}</p>
-            <p>{price}</p>
-            <p>{material}</p>
-            <ul>
-                <li>{size_height}</li>
-                <li>{size_width}</li>
-                <li>{size_deep}</li>
-            </ul>
-            <button>{/*add to cart*/}</button>
+        {displayProducts()}
         </div>
-    );
+    )
 
 };
 
 export default Furniture;
+
+// return (
+    //     <div>
+    //         <p>ceci est un meuble</p>
+    //         <img src={picture} alt="products"/>
+    //         <h1>{type}</h1>
+    //         <p>{description}</p>
+    //         <p>{price}</p>
+    //         <p>{material}</p>
+    //         <ul>
+    //             <li>{size_height}</li>
+    //             <li>{size_width}</li>
+    //             <li>{size_deep}</li>
+    //         </ul>
+    //         <button>{/*add to cart*/}</button>
+    //     </div>
+    // );
+
+//picture, type, description, price, material, size_height, size_width, size_deep
