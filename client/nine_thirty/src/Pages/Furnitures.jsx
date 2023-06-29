2d727fce0090e07610c406180d49b9105880c504
la2ure <2avlaure@gmail.com>
2023-06-29 15:15:18 +0200

    add pages/Furnitures to commit

---
+++ client/nine_thirty/src/Pages/Furnitures.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import Furniture from '../Components/Furniture';
+
+function Furnitures () {
+
+    const [data, setData] = useState([])
+    
+    useEffect(() => {
+        const getData = () => {
+            fetch("https://649ada96bf7c145d02399a1b.mockapi.io/furniture/articles")
+            .then(response => response.json())
+            .then(dataLocal => {
+                setData(dataLocal);
+                console.log(dataLocal)
+            });
+    
+        };
+        getData();
+    }, []);
+
+    return (
+
+        <div>
+        
+            <p>Page Furnitures</p>
+
+                {data ? (
+                    data.map(element => (
+                    <Furniture 
+                        key={element.id} 
+                        type={element.type} 
+                        price={element.price + " €"}
+                        />
+                    ))
+                ) : null}
+        </div>
+    );
+    
+};
+
+export default Furnitures;
+
+// https://649ada96bf7c145d02399a1b.mockapi.io/furniture/articles
