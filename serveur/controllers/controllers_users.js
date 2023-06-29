f6d0d40da3c0ab238b639f1cb2b8f9d96d2e9e5a
Regis-75018 <regisdesmarais@hotmail.fr>
2023-06-29 15:54:48 +0200

    check if user already register

--- serveur/controllers/controllers_users.js
+++ serveur/controllers/controllers_users.js
@@ -4,21 +4,29 @@ const jwt = require("jsonwebtoken"); // JSON Web Token (JWT) is a compact and di
 require("dotenv").config(); // for import the environnement variable
 
 exports.signupUser = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new User({
-        name: req.body.name,
-        surname: req.body.surname,
-        email: req.body.email,
-        password: hash, // hash the password when the user is signed
-      });
-      user
-        .save()
-        .then(() => res.status(201).json({ message: "User created" }))
-        .catch((error) => res.status(400).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+  const email = req.body.email;
+  User.findOne({ email })
+  .then((existingUser) => {
+    if (existingUser) {
+      return res.status(409).json({ message: "email already exist" });
+    }
+
+    bcrypt
+      .hash(req.body.password, 10)
+      .then((hash) => {
+        const user = new User({
+          name: req.body.name,
+          surname: req.body.surname,
+          email: email,
+          password: hash, // hash the password when the user is signed
+        });
+        user
+          .save()
+          .then(() => res.status(201).json({ message: "User created" }))
+          .catch((error) => res.status(400).json({ error }));
+      })
+      .catch((error) => res.status(500).json({ error }));
+  });
 };
 
 exports.loginUser = (req, res, next) => {
@@ -35,9 +43,10 @@ exports.loginUser = (req, res, next) => {
           }
           res.status(200).json({
             message: "Successfully connected", // A success message indicating a successful connection
-            isAdmin: user.isAdmin, 
+            isAdmin: user.isAdmin,
             userId: user._id, // The unique identifier of the user who connected successfully
-            token: jwt.sign( // Generating a JSON Web Token (JWT) for the user
+            token: jwt.sign(
+              // Generating a JSON Web Token (JWT) for the user
               { userId: user._id }, // Payload of the JWT containing the user's ID
               process.env.JWT_SECRET, // Secret key used to sign the JWT
               { expiresIn: "24h" } // Expiration time for the JWT (set to 24 hours)
