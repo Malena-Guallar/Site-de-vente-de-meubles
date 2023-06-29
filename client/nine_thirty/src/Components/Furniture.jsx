47af7133c145b1d0e910525e96b1679086f1621b
Malena Guallar <malena.guallar@gmail.com>
2023-06-29 15:56:24 +0200

    fixing furniture props

--- client/nine_thirty/src/Components/Furniture.jsx
+++ client/nine_thirty/src/Components/Furniture.jsx
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function Furniture ({ type, price }) {
-
-    getProducts();
+function Furniture ({ type, price, description, picture, material, size_deep, size_height, size_width }) {
 
     return (
         <div>
@@ -24,22 +22,3 @@ function Furniture ({ type, price }) {
 };
 
 export default Furniture;
-
-// return (
-    //     <div>
-    //         <p>ceci est un meuble</p>
-    //         <img src={picture} alt="products"/>
-    //         <h1>{type}</h1>
-    //         <p>{description}</p>
-    //         <p>{price}</p>
-    //         <p>{material}</p>
-    //         <ul>
-    //             <li>{size_height}</li>
-    //             <li>{size_width}</li>
-    //             <li>{size_deep}</li>
-    //         </ul>
-    //         <button>{/*add to cart*/}</button>
-    //     </div>
-    // );
-
-//picture, type, description, price, material, size_height, size_width, size_deep
