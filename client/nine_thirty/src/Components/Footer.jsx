33bb00f84630cb9e474a74ea91a3fcc67371ece0
la2ure <2avlaure@gmail.com>
2023-06-29 15:06:18 +0200

    creation of footer component

---
+++ client/nine_thirty/src/Components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Footer() {
+    return(
+        
+        <footer className="fixed bottom-0 bg-gray-900 text-white">
+            <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7"></div>
+        
+            <p> Who are we ? </p>
+            <p>FAQ</p>
+            <p>GCU</p>
+            <p>Contact</p>
+
+        </footer>
+        
+
+    )
+};
+
+export default Footer;
