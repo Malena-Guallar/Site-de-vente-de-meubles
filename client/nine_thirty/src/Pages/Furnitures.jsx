785498c6ffa4a77133461f2d6ae2ebca472dfcee
Malena Guallar <malena.guallar@gmail.com>
2023-06-29 14:12:02 +0200

    fetching api, displaying products with map

--- client/nine_thirty/src/Pages/Furnitures.jsx
+++ client/nine_thirty/src/Pages/Furnitures.jsx
@@ -2,9 +2,21 @@ import React, { useEffect, useState } from 'react';
 import Furniture from '../Components/Furniture';
 
 function Furnitures () {
+
+    const [data, setData] = useState([])
     
-
-
+    useEffect(() => {
+        const getData = () => {
+            fetch("https://649ada96bf7c145d02399a1b.mockapi.io/furniture/articles")
+            .then(response => response.json())
+            .then(dataLocal => {
+                setData(dataLocal);
+                console.log(dataLocal)
+            });
+    
+        };
+        getData();
+    }, []);
 
     return (
 
@@ -12,6 +24,11 @@ function Furnitures () {
         
             <p>Page Furnitures</p>
 
+                {data ? (
+                    data.map(element => (
+                    <Furniture key={element.id} type={element.type} />
+                    ))
+                ) : null}
         </div>
     );
     
