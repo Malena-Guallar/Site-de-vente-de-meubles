3cff20db158224245027a8a543159d7e071aa423
Malena Guallar <malena.guallar@gmail.com>
2023-07-03 10:18:57 +0200

    idk

--- client/nine_thirty/.vite/deps/react-dom_client.js
+++ client/nine_thirty/.vite/deps/react-dom_client.js
@@ -1,7 +1,7 @@
 import {
   __commonJS,
   require_react
-} from "./chunk-V42YN7MM.js";
+} from "./chunk-QSQYAWSL.js";
 
 // node_modules/scheduler/cjs/scheduler.development.js
 var require_scheduler_development = __commonJS({
