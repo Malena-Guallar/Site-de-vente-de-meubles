0f40bca1ae196e01107aed252ebb6de680cc2b09
Djeneba Soumare <djenebasoum98@icloud.com>
2023-07-03 15:46:39 +0200

    maj front befor merge

--- client/nine_thirty/.vite/deps/react.js
+++ client/nine_thirty/.vite/deps/react.js
@@ -1,5 +1,5 @@
 import {
   require_react
-} from "./chunk-QSQYAWSL.js";
+} from "./chunk-V42YN7MM.js";
 export default require_react();
 //# sourceMappingURL=react.js.map
--- client/nine_thirty/src/Components/Identification.jsx
+++ client/nine_thirty/src/Components/Identification.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-function Identification () {
-    return (
+function Identification() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check user's session status
+    const sessionData = sessionStorage.getItem("User");
+    if (sessionData) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    // Clear user's session
+    sessionStorage.removeItem("User");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div>
+      {isLoggedIn ? (
         <div>
-            <img class="h-6" src="connection.png" alt="icone connection" />
+          <p>Logged in as {sessionStorage.getItem("User")}</p>
+          <button onClick={handleLogout}>
+            
+            <img
+          className="h-6"
+          src="Logout.png"
+          alt="icon logout"
+        /> 
+             </button>
         </div>
-    );
-};
+      ) : (
+    <Link to="/Login">
+      <div>
+        <img
+          className="h-6"
+          src="connection.png"
+          alt="icone Log In"
+        /> 
+      </div>
+    </Link>
+      )}
+     </div>
+  )
+ 
+}
 
 export default Identification;
