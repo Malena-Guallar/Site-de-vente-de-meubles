9ea900b2b963c4adde811f963b78469bc1ccaa96
Malena Guallar <malena.guallar@gmail.com>
2023-07-05 13:55:28 +0200

    comments on code

--- serveur/app.test.js
+++ serveur/app.test.js
@@ -2,6 +2,8 @@ const request = require("supertest");
 const app = require('./app');
 const mongoose = require("mongoose");
 
+// Fonction pour se connecter à la base de données avant le test. 
+// Nécessaire car le test ne peut pas run sur des fonctions en lien avec la bdd. 
 beforeEach(async () => {
     await mongoose.connect(process.env.DB_MONGO, {
         useNewUrlParser: true,
@@ -14,10 +16,12 @@ beforeEach(async () => {
 //     .get("/users/649ecfff30ee3a0d92d015f4")
 // })
 
+// Fonction pour se déconnecter de la bdd après le test. 
 afterEach(async () => {
     await mongoose.connection.close();
 })
 
+// Ici test de la fonction getName. 
 describe('getName', () => {
     it('should return a user name', async () => {
         const res = await request(app.app).get("/users/649ecfff30ee3a0d92d015f4")
