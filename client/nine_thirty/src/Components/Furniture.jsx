69dff6c3eb9f27dd0c50dc1e9090d756e78e69e0
Djeneba Soumare <djenebasoum98@icloud.com>
2023-07-05 20:41:09 +0200

    add style to admin cart and a message when product added to cart

--- client/nine_thirty/src/Components/Furniture.jsx
+++ client/nine_thirty/src/Components/Furniture.jsx
@@ -1,48 +1,74 @@
-// Composant enfant qui va être importé dans la page Furnitures. 
+// Composant enfant qui va être importé dans la page Furnitures.
 
-import React, {useState, useEffect, useContext} from 'react';
-import AddtoCartButton from './AddtoCartButton';
-import { CartContext } from '../App';
+import React, { useState, useEffect, useContext } from "react";
+import AddtoCartButton from "./AddtoCartButton";
+import { CartContext } from "../App";
 
-// ici on défini toutes les props qui composent chaque produit. 
-function Furniture ({ type, price, description, picture, material, size_deep, size_height, size_width, height, width }) {
+// ici on défini toutes les props qui composent chaque produit.
+function Furniture({
+  type,
+  price,
+  description,
+  picture,
+  material,
+  size_deep,
+  size_height,
+  size_width,
+  height,
+  width,
+}) {
+  const { setCart } = useContext(CartContext);
+  const { cart } = useContext(CartContext);
+  const [showModal, setShowModal] = useState(false);
 
-    
-    const {setCart}  = useContext(CartContext);
-    const {cart} = useContext(CartContext);
+  // const [cart, setCart] = useState([]);
 
-    // const [cart, setCart] = useState([]);
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
-
-    useEffect(() => {
-        localStorage.setItem('cart', JSON.stringify(cart));
-    }, [cart]);
-
-
-    const handleAddToCart = () => {
-
-            const newItem = { type, price, description, picture };
-            setCart([...cart, newItem]);
-    }; 
-
-    
-    return (
-        <div style={{ height: height, width: width }}>
-            {/* <p>ceci est un meuble</p> */}
-            <img src={picture} alt="products picture" className='h-3/4 w-full text-center mb-1'/>
-            <h1>Type:{type}</h1>
-            {/* <p className='truncate'>Description: {description}</p> */}
-            <p>Price: {price}</p>
-            {/* <p>Material: {material}</p> */}
-            <ul>
-                {/* <li>Height: {size_height}</li> */}
-                {/* <li>Width: {size_width}</li> */}
-                {/* <li>Deep: {size_deep}</li> */}
-            </ul>
-                <AddtoCartButton onClick={handleAddToCart}/>
-        </div>
-    );
-
+  const handleAddToCart = () => {
+    const newItem = { type, price, description, picture };
+    setCart([...cart, newItem]);
+    setShowModal(true);
+  };
+  const closeModal = () => {
+    setShowModal(false);
 };
+  return (
+    
+    <div style={{ height: height, width: width }}>
+      {/* <p>ceci est un meuble</p> */}
+      <img
+        src={picture}
+        alt="products picture"
+        className="h-3/4 w-full text-center mb-1"
+      />
+      <h1>Type:{type}</h1>
+      {/* <p className='truncate'>Description: {description}</p> */}
+      <p>Price: {price}</p>
+      {/* <p>Material: {material}</p> */}
+      <ul>
+        {/* <li>Height: {size_height}</li> */}
+        {/* <li>Width: {size_width}</li> */}
+        {/* <li>Deep: {size_deep}</li> */}
+      </ul>
+      <AddtoCartButton onClick={handleAddToCart} />
+      {showModal && (
+                <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+                    <div className="bg-white p-4 rounded-lg">
+                        <p className="text-lg font-semibold">You added {type} to cart!</p>
+                        <button
+                            className="bg-[#2E2E68] text-white px-4 py-2 rounded mt-4"
+                            onClick={closeModal}
+                        >
+                            Close
+                        </button>
+                    </div>
+                </div>
+            )}
+    </div>
+  );
+}
 
 export default Furniture;
--- serveur/models/models_articles.js
+++ serveur/models/models_articles.js
@@ -2,31 +2,19 @@
 const mongoose = require("mongoose");
 // ****************************champs souhaites avec la methode Schema
 const Article = mongoose.Schema({
-  type: { type: String, required: false },
+type: { type: String, required: true },
   status: { type: String, required: false },
-  color: { type: Array, required: false },
-  material: { type: Array, required: false },
-  description: { type: String, required: false },
-  price: { type: Number, required: false },
-  quantity: { type: Number, required: false },
+  color: { type: Array, required: true },
+  material: { type: Array, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
   size_height: { type: Number, required: false },
   size_width: { type: Number, required: false },
   size_deep: { type: Number, required: false },
   picture: { type: Object, required: false },
-  seller: { type: String, required: false },
+  seller: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Article", Article);
 
-// type: { type: String, required: true },
-//   status: { type: String, required: false },
-//   color: { type: Array, required: true },
-//   material: { type: Array, required: true },
-//   description: { type: String, required: true },
-//   price: { type: Number, required: true },
-//   quantity: { type: Number, required: true },
-//   size_height: { type: Number, required: false },
-//   size_width: { type: Number, required: false },
-//   size_deep: { type: Number, required: false },
-//   picture: { type: Object, required: false },
-//   seller: { type: String, required: true },
